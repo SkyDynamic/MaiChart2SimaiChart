cca23651e8e45238b6f8e5f196b6d3a35abc41d5
NuanRMxi <2308425927@qq.com>
2025-06-03 06:12:57 +0800

    feat: Process bar

--- MaiChart2SimaiChart/Commands/CompileDatabase.cs
+++ MaiChart2SimaiChart/Commands/CompileDatabase.cs
@@ -10,19 +10,19 @@ public class CompileDatabase : ConsoleCommand
     public const int Failed = 2;
     public bool StrictDecimal { get; set; }
     public bool MusicIdFolderName { get; set; }
-    
+
     public string? A000Location { get; set; }
-    
+
     public string? Difficulty { get; set; }
-    
+
     public int CategorizeIndex { get; set; }
-    
+
     public string? Destination { get; set; }
-    
+
     public string CategorizeMethods { get; set; }
-    
+
     public string? Rotate { get; set; }
-    
+
     public int? ShiftTick { get; set; }
 
     public CompileDatabase()
@@ -32,9 +32,9 @@ public class CompileDatabase : ConsoleCommand
         {
             CategorizeMethods += $"[{i}]{Program.TrackCategorizeMethodSet[i]}\n";
         }
-        
+
         IsCommand("CompileDatabase", "Compile whole ma2 database to format assigned");
-        
+
         HasLongDescription(
             "This function enables user to compile whole database to the format they want. By default is simai for ma2.");
         HasRequiredOption("p|path=", "REQUIRED: Folder of A000 to override - end with a path separator",
@@ -50,32 +50,32 @@ public class CompileDatabase : ConsoleCommand
         HasOption("d|decimal:", "Force output chart to have levels rated by decimal", _ => StrictDecimal = true);
         HasOption("n|number:", "Use musicID as folder name instead of sort name", _ => MusicIdFolderName = true);
     }
-    
+
     public override int Run(string[] remainingArguments)
     {
         try
         {
             string a000Location =
                 A000Location ?? throw new FileNotFoundException("A000 location was not specified");
-            
+
             if (a000Location.Equals(""))
             {
                 a000Location = Program.DefaultPaths[0];
             }
-            
+
             string musicLocation = $"{a000Location}/music/";
             string originSoundLocation = $"{a000Location}/SoundData";
             string originImageLocation = $"{a000Location}/AssetBundleImages/jacket";
             string originVideoLocation = $"{a000Location}/MovieData";
-            
+
             string[] musicFolders = Directory.GetDirectories(musicLocation);
-            
+
             string outputLocation = Destination ?? throw new NullReferenceException("Destination not specified");
             if (outputLocation.Equals(""))
             {
                 outputLocation = Program.DefaultPaths[4];
             }
-            
+
             try
             {
                 if (0 <= CategorizeIndex && CategorizeIndex < Program.TrackCategorizeMethodSet.Length)
@@ -85,104 +85,105 @@ public class CompileDatabase : ConsoleCommand
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message +
-                                  " The program will use Genre as default method. Press any key to continue.");
+                Console.WriteLine(e.Message + " The program will use Genre as default method. Press any key to continue.");
                 Program.GlobalTrackCategorizeMethod = Program.TrackCategorizeMethodSet[0];
                 CategorizeIndex = 0;
                 Console.ReadKey();
             }
 
-            foreach (var track in musicFolders)
+            int numberOfTracks = musicFolders.Length;
+            using (var progressBar = new ConsoleProgressBar(numberOfTracks))
             {
-                Console.WriteLine("Iterating on folder {0}", track);
-                // Check the file status
-                string[] files = Directory.GetFiles(track);
-                if (files.Length <= 1)
+                foreach (var track in musicFolders)
                 {
-                    Console.WriteLine("Not enough files in the folder, skipping track: ", track);
-                    continue; 
-                }
-                if (File.Exists($"{track}/Music.xml"))
-                {
-                    TrackInformation trackInfo = new XmlInformation($"{track}/");
-                    Console.WriteLine("There is Music.xml in {0}", track);
-                    string shortID = Program.CompensateZero(trackInfo.TrackID).Substring(2);
-                    Console.WriteLine($"Name: {trackInfo.TrackName}");
-                    Console.WriteLine($"ID: {trackInfo.TrackID}");
-                    Console.WriteLine($"Genre: {trackInfo.TrackGenre}");
-                    string[] categorizeScheme =
-                    [
-                        trackInfo.TrackGenre, trackInfo.TrackSymbolicLevel, trackInfo.TrackVersion,
-                        trackInfo.TrackComposer, trackInfo.TrackBPM, trackInfo.StandardDeluxePrefix, ""
-                    ];
-                    string defaultCategorizedPath = $"{outputLocation}/{categorizeScheme[CategorizeIndex]}";
-                    
-                    string trackNameSubstitute = MusicIdFolderName
-                        ? trackInfo.TrackID
-                        : $"{trackInfo.TrackID}_{trackInfo.TrackSortName}";
-                    
-                    if (!Directory.Exists(defaultCategorizedPath))
+                    Console.WriteLine("Iterating on folder {0}", track);
+                    // Check the file status
+                    string[] files = Directory.GetFiles(track);
+                    if (files.Length <= 1)
                     {
-                        Directory.CreateDirectory(defaultCategorizedPath);
-                        Console.WriteLine("Created folder: {0}", defaultCategorizedPath);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Already exist folder: {0}", defaultCategorizedPath);
-                    }
-                    
-                    string trackPath = MusicIdFolderName
-                        ? $"{defaultCategorizedPath}/{trackNameSubstitute}"
-                        : $"{defaultCategorizedPath}/{trackNameSubstitute}{trackInfo.DXChartTrackPathSuffix}";
-                    
-                    if (!Directory.Exists(trackPath))
-                    {
-                        Directory.CreateDirectory(trackPath);
-                        Console.WriteLine("Created song folder: {0}", trackPath);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Already exist song folder: {0}", trackPath);
+                        Console.WriteLine("Not enough files in the folder, skipping track: {0}", track);
+                        progressBar.Update();
+                        continue;
                     }
 
-                    SimaiCompiler compiler;
-                    if (trackInfo.InformationDict["Utage"] != "")
+                    if (File.Exists(Path.Combine(track,"Music.xml")))
                     {
-                        compiler = new SimaiCompiler(StrictDecimal, $"{track}/",
-                            $"{defaultCategorizedPath}/{trackNameSubstitute}_Utage", true);
-                        compiler.WriteOut(trackPath, true);
+                        TrackInformation trackInfo = new XmlInformation($"{track}/");
+                        Console.WriteLine("There is Music.xml in {0}", track);
+                        string shortID = Program.CompensateZero(trackInfo.TrackID).Substring(2);
+                        Console.WriteLine($"Name: {trackInfo.TrackName}");
+                        Console.WriteLine($"ID: {trackInfo.TrackID}");
+                        Console.WriteLine($"Genre: {trackInfo.TrackGenre}");
+                        string[] categorizeScheme =
+                        [
+                            trackInfo.TrackGenre, trackInfo.TrackSymbolicLevel, trackInfo.TrackVersion,
+                            trackInfo.TrackComposer, trackInfo.TrackBPM, trackInfo.StandardDeluxePrefix, ""
+                        ];
+                        var defaultCategorizedPath = Path.Combine(outputLocation, categorizeScheme[CategorizeIndex]);
+
+                        string trackNameSubstitute = MusicIdFolderName
+                            ? trackInfo.TrackID
+                            : $"{trackInfo.TrackID}_{trackInfo.TrackSortName}";
+
+                        if (!Directory.Exists(defaultCategorizedPath))
+                        {
+                            Directory.CreateDirectory(defaultCategorizedPath);
+                            Console.WriteLine("Created folder: {0}", defaultCategorizedPath);
+                        }
+                        else Console.WriteLine("Already exist folder: {0}", defaultCategorizedPath);
+                        
+                        
+                        var trackPath = MusicIdFolderName
+                            ? Path.Combine(defaultCategorizedPath, trackNameSubstitute)
+                            : Path.Combine(defaultCategorizedPath, trackNameSubstitute + trackInfo.DXChartTrackPathSuffix);
+
+                        if (!Directory.Exists(trackPath))
+                        {
+                            Directory.CreateDirectory(trackPath);
+                            Console.WriteLine("Created song folder: {0}", trackPath);
+                        }
+                        else Console.WriteLine("Already exist song folder: {0}", trackPath);
+                        
+
+                        SimaiCompiler compiler;
+                        if (trackInfo.InformationDict["Utage"] != "")
+                        {
+                            //compiler = new SimaiCompiler(StrictDecimal, $"{track}/", $"{defaultCategorizedPath}/{trackNameSubstitute}_Utage", true);
+                            compiler = new SimaiCompiler(StrictDecimal, Path.Combine(track),Path.Combine(defaultCategorizedPath,trackNameSubstitute + "_Utage"),true);
+                            compiler.WriteOut(trackPath, true);
+                        }
+                        else
+                        {
+                            compiler = new SimaiCompiler(StrictDecimal, Path.Combine(track), trackPath);
+                            compiler.WriteOut(trackPath, true);
+                            Program.CompiledChart.Add(compiler.GenerateOneLineSummary());
+                        }
+
+                        Console.WriteLine("Finished compiling maidata {0} to: {1}", trackInfo.TrackName, Path.Combine(trackPath,"maidata.txt"));
+
+                        Console.WriteLine($"Convert music {trackInfo.TrackName}");
+                        var wavPath = Task.Run(() =>
+                                AudioConvert.GetCachedWavPath(originSoundLocation, int.Parse(trackInfo.TrackID)))
+                            .GetAwaiter()
+                            .GetResult();
+                        AudioConvert.ConvertWavPathToMp3Stream(
+                            wavPath,
+                            new FileStream(Path.Combine(trackPath,"music.mp3"), FileMode.Create));
+
+                        Console.WriteLine($"Convert jacket {trackInfo.TrackName}");
+                        var img = ImageConvert.GetMusicJacketPngData(
+                            $"{originImageLocation}/ui_jacket_{int.Parse(trackInfo.TrackID) % 10000:000000}.ab");
+                        if (img is not null) File.WriteAllBytes(Path.Combine(trackPath,"bg.png"), img);
                     }
                     else
                     {
-                        compiler = new SimaiCompiler(StrictDecimal, $"{track}/", trackPath);
-                        compiler.WriteOut(trackPath, true);
-                        Program.CompiledChart.Add(compiler.GenerateOneLineSummary());
+                        Console.WriteLine("There is no Music.xml in folder {0}", track);
                     }
-                    
-                    Console.WriteLine("Finished compiling maidata {0} to: {1}", trackInfo.TrackName,
-                        $"{trackPath}/maidata.txt");
-                    
-                    Console.WriteLine($"Convert music {trackInfo.TrackName}");
-                    var wavPath = Task.Run(() => AudioConvert.GetCachedWavPath(originSoundLocation, int.Parse(trackInfo.TrackID)))
-                        .GetAwaiter()
-                        .GetResult();
-                    AudioConvert.ConvertWavPathToMp3Stream(
-                        wavPath,
-                        new FileStream($"{trackPath}/music.mp3", FileMode.Create));
-                    
-                    Console.WriteLine($"Convert jacket {trackInfo.TrackName}");
-                    byte[] img = ImageConvert.GetMusicJacketPngData($"{originImageLocation}/ui_jacket_{(int.Parse(trackInfo.TrackID) % 10000):000000}.ab");
-                    if (img is not null)
-                    {
-                        File.WriteAllBytes($"{trackPath}/bg.png", img);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("There is no Music.xml in folder {0}", track);
+                    progressBar.Update();
                 }
             }
-            
+
+
             Console.WriteLine("Total music compiled: {0}", Program.NumberTotalTrackCompiled);
             int index = 1;
             foreach (KeyValuePair<int, string> pair in Program.CompiledTracks)
@@ -190,8 +191,10 @@ public class CompileDatabase : ConsoleCommand
                 Console.WriteLine($"[{index}]: {pair.Key} {pair.Value}");
                 index++;
             }
+
             return Success;
-        } catch (Exception ex)
+        }
+        catch (Exception ex)
         {
             Console.WriteLine("Program cannot proceed because of following error returned: \n{0}", ex.GetType());
             Console.Error.WriteLine(ex.Message);
---
+++ MaiChart2SimaiChart/ConsoleProcessBar.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MaiChart2SimaiChart;
+
+public class ConsoleProgressBar : IDisposable
+{
+    private readonly int _total;
+    private int _current;
+    private readonly object _lock = new();
+    private readonly TextWriter _originalOut;
+    private readonly InterceptingTextWriter _interceptingWriter;
+    private bool _disposed;
+
+    public ConsoleProgressBar(int total)
+    {
+        if (total <= 0)
+            throw new ArgumentException("The total progress must be greater than 0", nameof(total));
+
+        _total = total;
+        _current = 0;
+
+        // 保存原始的Console.Out并设置拦截器
+        _originalOut = Console.Out;
+        _interceptingWriter = new InterceptingTextWriter(this, _originalOut);
+        Console.SetOut(_interceptingWriter);
+
+        // 初始化进度条显示
+        DrawProgressBar();
+    }
+
+    public void Update(int progress = 1)
+    {
+        lock (_lock)
+        {
+            if (_disposed) return;
+
+            _current = Math.Min(_current + progress, _total);
+            DrawProgressBar();
+        }
+    }
+
+    public void SetProgress(int progress)
+    {
+        lock (_lock)
+        {
+            if (_disposed) return;
+
+            _current = Math.Max(0, Math.Min(progress, _total));
+            DrawProgressBar();
+        }
+    }
+
+    internal void HandleConsoleOutput(string text)
+    {
+        lock (_lock)
+        {
+            if (_disposed) return;
+
+            // 清除进度条
+            ClearProgressBar();
+
+            // 输出被拦截的文本到原始输出
+            _originalOut.Write(text);
+
+            // 重新绘制进度条
+            DrawProgressBar();
+        }
+    }
+
+    private void DrawProgressBar()
+    {
+        if (_disposed) return;
+
+        // 保存当前光标位置
+        int currentTop = Console.CursorTop;
+        int currentLeft = Console.CursorLeft;
+
+        try
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+
+            double percentage = (double)_current / _total;
+
+            int consoleWidth = Console.WindowWidth;
+
+            string percentText = $" {percentage:P1}";
+            int percentWidth = percentText.Length;
+
+            int availableWidth = consoleWidth - 2 - percentWidth; // 2 for [ and ]
+
+            if (availableWidth > 0)
+            {
+                // 计算填充字符数量
+                int filledWidth = (int)(percentage * availableWidth);
+                int emptyWidth = availableWidth - filledWidth;
+
+                // 构建进度条
+                string progressBar = "[";
+
+                if (filledWidth > 0)
+                {
+                    // 添加已完成部分（=）和进度指示符（>）
+                    if (filledWidth == 1 && _current < _total)
+                    {
+                        progressBar += ">";
+                    }
+                    else if (_current < _total && filledWidth > 1)
+                    {
+                        progressBar += new string('=', filledWidth - 1) + ">";
+                    }
+                    else
+                    {
+                        progressBar += new string('=', filledWidth);
+                    }
+                }
+
+                // 添加空白部分
+                progressBar += new string(' ', emptyWidth);
+                progressBar += "]";
+                progressBar += percentText;
+
+                // 直接写入原始输出，避免递归拦截
+                _originalOut.Write(progressBar);
+            }
+        }
+        finally
+        {
+            // 如果原来的光标位置不是最后一行，则恢复光标位置
+            if (currentTop < Console.WindowHeight - 1)
+            {
+                Console.SetCursorPosition(currentLeft, currentTop);
+            }
+        }
+    }
+
+    private void ClearProgressBar()
+    {
+        if (_disposed) return;
+
+        // 保存当前光标位置
+        var currentTop = Console.CursorTop;
+        var currentLeft = Console.CursorLeft;
+
+        try
+        {
+            // 移动到控制台底部并清除
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            _originalOut.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        }
+        finally
+        {
+            // 恢复光标位置
+            if (currentTop < Console.WindowHeight - 1)
+            {
+                Console.SetCursorPosition(currentLeft, currentTop);
+            }
+        }
+    }
+
+    public void Complete()
+    {
+        SetProgress(_total);
+    }
+
+    public void Clear()
+    {
+        lock (_lock)
+        {
+            if (_disposed) return;
+
+            ClearProgressBar();
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        lock (_lock)
+        {
+            if (_disposed) return;
+
+            // 清除进度条
+            ClearProgressBar();
+
+            // 恢复原始的Console.Out
+            Console.SetOut(_originalOut);
+
+            // 释放拦截器
+            _interceptingWriter?.Dispose();
+
+            _disposed = true;
+        }
+    }
+
+    public bool IsCompleted => _current >= _total;
+    public int Current => _current;
+    public int Total => _total;
+    public double Percentage => (double)_current / _total;
+}
+
+// 拦截Console输出的TextWriter
+internal class InterceptingTextWriter : TextWriter
+{
+    private readonly ConsoleProgressBar _progressBar;
+    private readonly TextWriter _originalWriter;
+
+    public InterceptingTextWriter(ConsoleProgressBar progressBar, TextWriter originalWriter)
+    {
+        _progressBar = progressBar;
+        _originalWriter = originalWriter;
+    }
+
+    public override Encoding Encoding => _originalWriter.Encoding;
+
+    public override void Write(char value)
+    {
+        _progressBar.HandleConsoleOutput(value.ToString());
+    }
+
+    public override void Write(string? value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            _progressBar.HandleConsoleOutput(value);
+        }
+    }
+
+    public override void Write(char[]? buffer, int index, int count)
+    {
+        if (buffer == null || count <= 0) return;
+
+        var text = new string(buffer, index, count);
+        _progressBar.HandleConsoleOutput(text);
+    }
+
+    public override void WriteLine()
+    {
+        _progressBar.HandleConsoleOutput(Environment.NewLine);
+    }
+
+    public override void WriteLine(string? value)
+    {
+        _progressBar.HandleConsoleOutput(value + Environment.NewLine);
+    }
+
+    public override void WriteLine(char value)
+    {
+        _progressBar.HandleConsoleOutput(value + Environment.NewLine);
+    }
+
+    public override void WriteLine(char[]? buffer, int index, int count)
+    {
+        if (buffer != null && count > 0)
+        {
+            string text = new string(buffer, index, count);
+            _progressBar.HandleConsoleOutput(text + Environment.NewLine);
+        }
+    }
+
+    // 重写其他常用的Write方法
+    public override void Write(bool value) => Write(value.ToString());
+    public override void Write(int value) => Write(value.ToString());
+    public override void Write(uint value) => Write(value.ToString());
+    public override void Write(long value) => Write(value.ToString());
+    public override void Write(ulong value) => Write(value.ToString());
+    public override void Write(float value) => Write(value.ToString());
+    public override void Write(double value) => Write(value.ToString());
+    public override void Write(decimal value) => Write(value.ToString());
+    public override void Write(object? value) => Write(value?.ToString() ?? "");
+
+    public override void WriteLine(bool value) => WriteLine(value.ToString());
+    public override void WriteLine(int value) => WriteLine(value.ToString());
+    public override void WriteLine(uint value) => WriteLine(value.ToString());
+    public override void WriteLine(long value) => WriteLine(value.ToString());
+    public override void WriteLine(ulong value) => WriteLine(value.ToString());
+    public override void WriteLine(float value) => WriteLine(value.ToString());
+    public override void WriteLine(double value) => WriteLine(value.ToString());
+    public override void WriteLine(decimal value) => WriteLine(value.ToString());
+    public override void WriteLine(object? value) => WriteLine(value?.ToString() ?? "");
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // 不要释放原始writer，它可能仍在使用
+        }
+
+        base.Dispose(disposing);
+    }
+}
